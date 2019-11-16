edc75fe3acbcd2362ef0685d87c984deecb00dd1
Spencer Miskoviak <smiskoviak@gmail.com>
2019-11-16 13:20:14 -0800

    chore(example): add a basic example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,32 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+
+import { Img, resource } from '../src';
+
+const RANDOM_IMAGES: string[] = [];
+
+for (let i = 200; i < 250; i++) {
+  RANDOM_IMAGES.push(`https://placekitten.com/${i}/200`);
+}
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <React.SuspenseList revealOrder="forwards">
+        {RANDOM_IMAGES.map(src => {
+          resource.preloadImage(src);
+
+          return (
+            <React.Suspense fallback={null} key={src}>
+              <Img src={src} width={64} />
+            </React.Suspense>
+          );
+        })}
+      </React.SuspenseList>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.createRoot(root).render(<App />);
